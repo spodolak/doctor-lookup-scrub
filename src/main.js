d65ad82719a08c6402acf0fd53f34ffd2879e595
Student <example@example.com>
2020-02-14 14:09:28 -0800

    troubleshoot limited API results. hide API key

--- src/main.js
+++ src/main.js
@@ -5,7 +5,22 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles.css';
 
-let getElements = (response, searchInput) => {
+let showDoctorByName = (response, name) => {
+	for (var i = 0; i <= response.data.length; i += 1) {
+		console.log(response.data[i].profile);
+		let doctorSearch = response.data[i].profile.last_name;
+		if (name === doctorSearch) {
+			$("#result").append(`${response.data[i].profile.first_name} `);
+			$("#result").append(`${response.data[i].profile.last_name}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.street}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.city}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.state}<br>`);
+			$("#result").append(`${response.data[i].practices[0].phones[0].number}<br><br>`);
+		}
+	}
+}
+
+let showDoctorBySymptom = (response, searchInput) => {
 	for (var i = 1; i <= response.data.length; i += 1) {
 		console.log(response.data[i].profile);
 		let doctorSearch = response.data[i].profile.last_name;
@@ -20,15 +35,21 @@ let getElements = (response, searchInput) => {
 	}
 }
 
-$(document).ready(function () {
 
-	$("form#search-input").submit(function (event) {
+$(document).ready(function () {
+	$("form#search-name-input").submit(function (event) {
 		event.preventDefault();
-		let searchInput = "Johnson";
+		let name = $("#inputted-name").val();
+		let symptom $("")
 		(async () => {
 			let doctorLookup = new DoctorLookup();
-			const response = await doctorLookup.getDoctorByName();
-			getElements(response, searchInput);
+			const response = await doctorLookup.getDoctorByName(name);
+			showDoctorByName(response, name);
+		})();
+		(async () => {
+			let doctorLookup = new DoctorLookup();
+			const response = await doctorLookup.getDoctorBySymptom();
+			showDoctorBySymptom(response, searchInput);
 		})();
 	});
 });
