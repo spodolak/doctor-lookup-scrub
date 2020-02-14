6f220c67e00e3581755528ab3403504c84a6fca9
Student <example@example.com>
2020-02-14 11:42:01 -0800

    complete logic to loop through api and return search results

--- src/main.js
+++ src/main.js
@@ -6,29 +6,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles.css';
 
 let getElements = (response, searchInput) => {
-    for (var i = 1; i <= targetArray.length; i +=1) {
-      let doctorSearch = result.targetArray[i].targetAttribute];
-      if (searchInput === doctorSearch) {      
-      }
-    }
-  }
-  
-  $(document).ready(function()  {
-  
-    $("form#search-input").submit(function(event)  {
-      event.preventDefault();
-    //   let bikeColorInput = $("select#color").val();
-      (async () => {
-        let doctorLookup = new DoctorLookup();
-        const response = await doctorLookup.getDoctorByName();
-        console.log(response.data[0].profile);
-        $("#result").append(`${response.data[0].profile.first_name}`);
-        $("#result").append(`${response.data[0].profile.last_name}`);
-        $("#result").append(`${response.data[0].practices[0].visit_address.street}`);
-        $("#result").append(`${response.data[0].practices[0].visit_address.city}`);
-        $("#result").append(`${response.data[0].practices[0].visit_address.state}`);
-        $("#result").append(`${response.data[0].practices[0].phones[0].number}`);
-        getElements(response, searchInput);
-      })();
-    });
-  });
+	for (var i = 1; i <= response.data.length; i += 1) {
+		console.log(response.data[i].profile);
+		let doctorSearch = response.data[i].profile.last_name;
+		if (searchInput === doctorSearch) {
+			$("#result").append(`${response.data[i].profile.first_name} `);
+			$("#result").append(`${response.data[i].profile.last_name}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.street}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.city}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.state}<br>`);
+			$("#result").append(`${response.data[i].practices[0].phones[0].number}<br><br>`);
+		}
+	}
+}
+
+$(document).ready(function () {
+
+	$("form#search-input").submit(function (event) {
+		event.preventDefault();
+		let searchInput = "Johnson";
+		(async () => {
+			let doctorLookup = new DoctorLookup();
+			const response = await doctorLookup.getDoctorByName();
+			getElements(response, searchInput);
+		})();
+	});
+});
