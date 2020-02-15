1189725eba7be230ae26dbd6867eb748c8d4839f
Student <example@example.com>
2020-02-14 16:20:02 -0800

    add logic to return message to user if no doctors meet search criteria

--- src/main.js
+++ src/main.js
@@ -6,37 +6,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles.css';
 
 let showDoctorByName = (response) => {
-	for (var i = 0; i <= response.data.length; i += 1) {
-		$("#result").append(`${response.data[i].profile.first_name} `);
-		$("#result").append(`${response.data[i].profile.last_name}<br>`);
-		$("#result").append(`${response.data[i].practices[0].visit_address.street}<br>`);
-		$("#result").append(`${response.data[i].practices[0].visit_address.city}<br>`);
-		$("#result").append(`${response.data[i].practices[0].visit_address.state}<br>`);
-		$("#result").append(`${response.data[i].practices[0].phones[0].number}<br>`);
-		if (response.data[i].practices[0].website === undefined) {
-			$("#result").append("no website listed<br>");
-		} else {
-			$("#result").append(`${response.data[i].practices[0].website}<br>`);
+	if (response.data[0] === undefined) {
+		$("#result").append("I'm sorry, there are no doctors with that name in the Portland area!")
+	} else {
+		for (var i = 0; i <= response.data.length; i += 1) {
+			$("#result").append(`${response.data[i].profile.first_name} `);
+			$("#result").append(`${response.data[i].profile.last_name}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.street}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.city}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.state}<br>`);
+			$("#result").append(`${response.data[i].practices[0].phones[0].number}<br>`);
+			if (response.data[i].practices[0].website === undefined) {
+				$("#result").append("no website listed<br>");
+			} else {
+				$("#result").append(`${response.data[i].practices[0].website}<br>`);
+			}
+			$("#result").append(`${response.data[i].practices[0].accepts_new_patients}<br><br>`);
 		}
-		$("#result").append(`${response.data[i].practices[0].accepts_new_patients}<br><br>`);
 	}
 }
 
 let showDoctorBySymptom = (response) => {
-	for (var i = 0; i <= response.data.length; i += 1) {
-		$("#result").append(`${response.data[i].profile.first_name} `);
-		$("#result").append(`${response.data[i].profile.last_name}<br>`);
-		$("#result").append(`${response.data[i].practices[0].visit_address.street}<br>`);
-		$("#result").append(`${response.data[i].practices[0].visit_address.city}<br>`);
-		$("#result").append(`${response.data[i].practices[0].visit_address.state}<br>`);
-		$("#result").append(`${response.data[i].practices[0].phones[0].number}<br>`);
-		if (response.data[i].practices[0].website === undefined) {
-			$("#result").append("no website listed<br>");
-		} else {
+	if (response.data[0] === undefined) {
+		$("#result").append("I'm sorry, there are no doctors in the area that treat this symptom!")
+	} else {
+		for (var i = 0; i <= response.data.length; i += 1) {
+			$("#result").append(`${response.data[i].profile.first_name} `);
+			$("#result").append(`${response.data[i].profile.last_name}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.street}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.city}<br>`);
+			$("#result").append(`${response.data[i].practices[0].visit_address.state}<br>`);
+			$("#result").append(`${response.data[i].practices[0].phones[0].number}<br>`);
+			if (response.data[i].practices[0].website === undefined) {
+				$("#result").append("no website listed<br>");
+			} else {
+				$("#result").append(`${response.data[i].practices[0].website}<br>`);
+			}
 			$("#result").append(`${response.data[i].practices[0].website}<br>`);
+			$("#result").append(`${response.data[i].practices[0].accepts_new_patients}<br><br>`);
 		}
-		$("#result").append(`${response.data[i].practices[0].website}<br>`);
-		$("#result").append(`${response.data[i].practices[0].accepts_new_patients}<br><br>`);
 	}
 }
 
